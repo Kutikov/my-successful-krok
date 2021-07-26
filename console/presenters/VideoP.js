2e3ae20aa8f9b8ced80d65255042374ecad4c759
Damir A. Kutikov <damirkut@gmail.com>
2021-07-26 13:20:49 +0300

    working video visual edit

--- console/presenters/VideoP.js
+++ console/presenters/VideoP.js
@@ -5,6 +5,7 @@ class VideoP{
         this.endPoint = '00:00:00';
         this.isLocker = false;
         this.maxTime = 0;
+        this.videoTitle = '';
     }
 
     static PrepareEdit(props, contentL){
@@ -12,10 +13,11 @@ class VideoP{
         const videoURLTextEditTextArea = document.getElementById('videoURLTextEditTextArea');
         const videoFrom = document.getElementById('videoFrom');
         const videoTo = document.getElementById('videoTo');
+        const checkboxLockerVideo = document.getElementById('checkboxLockerVideo');
         videoFrom.value = props.startPoint;
         videoTo.value = props.endPoint;
         videoURLTextEditTextArea.value = 'https://www.youtube.com/watch?v=' + props.videoId;
-        document.getElementById('checkboxLockerVideo').checked = props.isLocker;
+        checkboxLockerVideo.checked = props.isLocker;
         videoURLTextEditTextArea.addEventListener('input', (e) => {
             currentPresenter.OnEditAction('videoURLTextEditTextArea', null);
         });
@@ -25,6 +27,9 @@ class VideoP{
         videoTo.addEventListener('blur', (e) => {
             currentPresenter.OnEditAction(null, null);
         });
+        checkboxLockerVideo.addEventListener('click', (e) => {
+            currentPresenter.OnEditAction(null, null);
+        })
         currentPresenter.OnEditAction('videoURLTextEditTextArea', null);
     }
 
@@ -50,6 +55,7 @@ class VideoP{
             if(videoId != tempProps.videoId || tempProps.maxTime == 0){
                 tempProps.videoId = videoId;
                 tempProps.maxTime = 0;
+                tempProps.videoTitle = '';
                 tempProps.startPoint = '00:00:00';
                 tempProps.endPoint = '00:00:00';
                 videoFrom.value = '00:00:00';
@@ -62,16 +68,17 @@ class VideoP{
                 const technicalVideoHolder2 = document.createElement('div');
                 technicalVideoHolder2.id = 'technicalVideoHolder2';
                 technicalVideoHolder.appendChild(technicalVideoHolder2);
-                const techno = new YT.Player('technicalVideoHolder2', {
+                new YT.Player('technicalVideoHolder2', {
                     height: '360',
                     width: '640',
                     videoId: tempProps.videoId,
                     events: {
                         'onReady': (event) => {
-                            techno.playVideo();
+                            event.target.setVolume(0);
+                            event.target.playVideo();
                             setTimeout(() => {
-                                techno.stopVideo();
-                                VideoP.OnEditAction(tempProps, null, techno.getDuration());
+                                event.target.stopVideo();
+                                currentPresenter.OnEditAction(null, {duration: Math.floor(event.target.getDuration()), title: event.target.getVideoData().title});
                             }, 5000);
                         },
                         'onStateChange': (event) => {}
@@ -84,10 +91,10 @@ class VideoP{
                 const startObj = VideoP.StringToSeconds(videoFrom.value);
                 const finishObj = VideoP.StringToSeconds(videoTo.value);
                 if(finishObj.success){
-                    if(tempProps.maxTime <= finishObj.seconds){
+                    if(tempProps.maxTime < finishObj.seconds){
                         const maxEndTime = VideoP.SecondToString(tempProps.maxTime);
                         videoTo.value = maxEndTime;
-                        VideoP.OnEditAction(tempProps, null, null);
+                        currentPresenter.OnEditAction(null, null);
                         return;
                     }
                     else{
@@ -97,22 +104,25 @@ class VideoP{
                 else{
                     const maxEndTime = VideoP.SecondToString(tempProps.maxTime);
                     videoTo.value = maxEndTime;
-                    VideoP.OnEditAction(tempProps, null, null);
+                    currentPresenter.OnEditAction(null, null);
                     return;
                 }
                 if(startObj.success){
                     if(tempProps.maxTime <= startObj.seconds){
                         const maxStartTime = VideoP.SecondToString(tempProps.maxTime - 1);
                         videoFrom.value = maxStartTime;
-                        VideoP.OnEditAction(tempProps, null, null);
+                        currentPresenter.OnEditAction(null, null);
                         return;
                     }
-                    if(VideoP.StringToSeconds(tempProps.endPoint) != 0){
+                    if(VideoP.StringToSeconds(tempProps.endPoint).seconds != 0){
                         if(startObj.seconds >= finishObj.seconds){
                             const maxStartTime = VideoP.SecondToString(finishObj.seconds - 1);
                             videoFrom.value = maxStartTime;
-                            VideoP.OnEditAction(tempProps, null, null);
+                            currentPresenter.OnEditAction(null, null);
                             return;
+                        }
+                        else{
+                            tempProps.startPoint = startObj.point;
                         }
                     }
                     else{
@@ -121,24 +131,25 @@ class VideoP{
                 }
                 else{
                     videoFrom.value = startObj.point;
-                    VideoP.OnEditAction(tempProps, null, null);
+                    currentPresenter.OnEditAction(null, null);
                     return;
                 }
             }            
         }
         else{
             linearProgress.style.display = 'none';
-            tempProps.maxTime = message;
+            tempProps.maxTime = message.duration;
+            tempProps.videoTitle = message.title;
             videoFrom.value = VideoP.SecondToString(0);
             videoTo.value = VideoP.SecondToString(tempProps.maxTime);
-            VideoP.OnEditAction(tempProps, null, null);
+            currentPresenter.OnEditAction(null, null);
         }
     }
 
     static SecondToString(sec){
         const hour = (sec - sec % 3600) / 3600; 
         const minutes = ((sec % 3600) - (sec % 3600) % 60) / 60;
-        const seconds = Math.floor(sec - hour * 3600 - minutes * 60);
+        const seconds = sec - hour * 3600 - minutes * 60;
         return hour.toString().padStart(2, '0') + ':' + minutes.toString().padStart(2, '0') + ':' + seconds.toString().padStart(2, '0');
     }
 
@@ -151,20 +162,18 @@ class VideoP{
             if(seconds > 59){
                 successL = false;
                 seconds = 59;
-                parts[2] = '59';
             }
             let minutes = new Number(parts[1]);
             if(minutes > 59){
                 successL = false;
                 minutes = 59;
-                parts[1] = '59';
             }
             let hours = new Number(parts[0]);
             let secondsConverted = 0;
             if(successL){
                 secondsConverted = seconds + minutes * 60 + hours * 60 * 60;
             }
-            return { success: successL, point: hours.toString() + ':' + minutes.toString() + ':' + seconds.toString(), seconds: secondsConverted };
+            return { success: successL, point: VideoP.SecondToString(secondsConverted), seconds: secondsConverted };
         }
         return { success: false, point: '00:00:00', seconds: 0 };
     }
@@ -181,49 +190,76 @@ class VideoP{
         const preview = document.createElement('div');
         const previewIcon = document.createElement('p');
         preview.id = 'preview' + Math.round(Math.random() * 100000).toString();
-        previewIcon.innerText = 'link';
+        preview.style.display = 'none';
+        previewHolder.classList.add('video__preview_holder');
+        previewIcon.innerText = 'hourglass_empty';
         previewIcon.classList.add('material-icons');
         previewIcon.classList.add('video__preview_icon');
+        previewIcon.id = preview.id + 'button';
         previewHolder.appendChild(preview);
         previewHolder.appendChild(previewIcon);
-        const techno = new YT.Player(preview.id, {
+        const startS = VideoP.StringToSeconds(props.startPoint).seconds;
+        const endS = VideoP.StringToSeconds(props.endPoint).seconds;
+        
+        const titlePar = document.createElement('p');
+        titlePar.classList.add('card__text');
+        titlePar.classList.add('par__textStyle_b');
+        titlePar.innerText = props.videoTitle;
+        const timePar = document.createElement('p');
+        timePar.classList.add('card__text');
+        timePar.innerText = 'С ' + props.startPoint + " по " + props.endPoint + " (всего " + VideoP.SecondToString(endS - startS) + ")." + (props.isLocker ? ' Блокирует дальнейший просмотр.' : '');
+        contentL.appendChild(previewHolder);
+        contentL.appendChild(titlePar);
+        contentL.appendChild(timePar);
+        const lockP = new YT.Player(preview.id, {
             height: '360',
             width: '640',
             videoId: props.videoId,
             playerVars: {
                 fs: 1,
-                end: VideoP.StringToSeconds(props.endPoint),
+                start: startS,
                 controls: 0,
-                autoplay: 1,
+                autoplay: 0,
                 disablekb: 1,
+                enablejsapi: 1,
                 rel: 0,
                 showinfo: 0,
-                start: VideoP.StringToSeconds(props.startPoint)
+                end: endS
             },
             events: {
                 'onReady': (event) => {
-                    previewIcon.style.display = 'block';
-                    preview.style.display = 'block';
+                    const previewL = document.getElementById(preview.id);
+                    const previewButtonL = document.getElementById(preview.id + 'button');
+                    previewButtonL.innerText = 'play_arrow';
+                    previewL.style.display = 'block';
+                    previewL.style.position = 'absolute';
+                    previewL.style.width = '100%';
+                    previewL.style.height = '100%';
+                    previewButtonL.addEventListener('click', function () {
+                        switch (lockP.getPlayerState()) {
+                            case 0:
+                            case -1:
+                            case 2:
+                            case 5:
+                                lockP.playVideo();
+                                previewButtonL.style.opacity = '0.0';
+                                break;
+                            case 1:
+                                lockP.pauseVideo();
+                                previewButtonL.style.opacity = '1.0';
+                                break;
+                        }
+                    });
                 },
                 'onStateChange': (event) => {
-                    
+                    const previewButtonL = document.getElementById(preview.id + 'button');
+                    if(lockP.getPlayerState() == 0){
+                        lockP.pauseVideo();
+                        previewButtonL.style.opacity = '1.0';
+                        lockP.seekTo(startS, true);
+                    }
                 }
             }
         });
-        previewHolder.addEventListener('click', function(){
-            switch(techno.getPlayerState()){
-                case 0:
-                case -1:
-                case 2:
-                    techno.playVideo();
-                    previewIcon.style.display = 'none';
-                    break;
-                case 1:
-                    techno.pauseVideo();
-                    previewIcon.style.display = 'block';
-                    break;
-            }
-        });
-        contentL.appendChild(previewHolder);
     }
 }
