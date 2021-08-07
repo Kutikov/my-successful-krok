3f3d7e9502489cf81c91d4495042a38dc33be7a7
Julia Kiziurina <juliibox@gmail.com>
2021-08-07 11:18:25 +0300

    remove reduntant objects

--- auth/User.js
+++ auth/User.js
@@ -1,27 +1,19 @@
-class User{
-    constructor(email, name){
+class User {
+    constructor(email, name) {
         this.email = email;
         this.name = name;
         this.buckets = [];
-        this.device1 = {
-            date: null,
-            data: ''
-        };
-        this.device2 = {
-            date: null,
-            data: ''
-        }
-        this.tempRequest = '';
+        this.device1 = null;
+        this.device2 = null
     }
 
-    GetFireStroreObject(){
+    GetFireStroreObject() {
         return {
             email: this.email,
             name: this.name,
-            buckets: [],
+            buckets: this.buckets,
             device1: this.device1,
-            device2: this.device2,
-            tempRequest: ''
+            device2: this.device2
         };
     }
 }
