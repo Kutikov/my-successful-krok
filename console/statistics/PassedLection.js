1e78e0f429c4ca6df1130d30382f03a924fc0bdc
Damir A. Kutikov <damirkut@gmail.com>
2021-10-22 23:10:57 +0300

    statistics tables

--- console/statistics/PassedLection.js
+++ console/statistics/PassedLection.js
@@ -7,8 +7,44 @@ class PassedLection{
         const thisItem = new PassedLection();
         thisItem.finishedOn = record.finishedOn;
         thisItem.startedOn = record.startedOn;
+        thisItem.duration = record.duration; 
         thisItem.unitId = record.fork_unitId.split("@")[0];
         thisItem.forkId = record.fork_unitId.split("@")[1];
         return thisItem;
+    }
+    static Draw(allPassedLections, forkId){
+        const passedLectionsHolder = document.getElementById('passedLectionsHolder');
+        while(passedLectionsHolder.firstChild){
+            passedLectionsHolder.remove(passedLectionsHolder.lastChild);
+        }
+        let r = 0;
+        for(let i = 0 ; i < allPassedLections.length; i++){
+            if(allPassedLections[i].forkId == forkId){
+                const passed = allPassedLections[i];
+                const item = document.createElement('div');
+                item.classList.add('lectionsRow');
+                const unitText = document.createElement('p');
+                unitText.classList.add('unitTextLections');
+                if(r % 2){
+                    unitText.classList.add('darkRow');
+                }
+                unitText.innerText = passed.unitId.replace(/ø/g, ' ');
+                const startedText = document.createElement('p');
+                startedText.className = 'startedTextLections';
+                startedText.innerText = Date.today().addMilliseconds(-new Date().getTime()).addMilliseconds(passed.startedOn).toString("d.MM.yy, HH:mm:ss");
+                const finishText = document.createElement('p');
+                finishText.className = 'finishedTextLections';
+                finishText.innerText = Date.today().addMilliseconds(-new Date().getTime()).addMilliseconds(passed.finishedOn).toString("d.MM.yy, HH:mm:ss");
+                const durationText = document.createElement('p');
+                durationText.className = 'durationTextLections';
+                durationText.innerText = Date.today().addMilliseconds(-new Date().getTime()).addMilliseconds(passed.duration).toString("HH:mm:ss");
+                item.append(unitText);
+                item.append(startedText);
+                item.append(finishText);
+                item.append(durationText);
+                passedLectionsHolder.append(item);
+                r++;
+            }
+        }
     }
 }
--- console/statistics/PassedTesting.js
+++ console/statistics/PassedTesting.js
@@ -8,14 +8,45 @@ class PassedTesting{
         thisItem.correctTests = record.correctTests;
         thisItem.allTests = record.allTests;
         thisItem.percent = Math.floor(record.correctTests / record.allTests * 100);
-        thisItem.duration = Math.floor(record.duration / 1000);
+        thisItem.duration = record.duration;
         thisItem.forkId = record.fork_unitId.split("@")[1];
         thisItem.unitId = record.fork_unitId.split("@")[0];
         thisItem.startedOn = record.startedOn;
         return thisItem;
     }
 
-    static GetDays(){
-        
+    static Draw(allPassedTestings, forkId){
+        const passedTestingsHolder = document.getElementById('passedTestingsHolder'); //TODO tbody
+        while(passedTestingsHolder.firstChild){
+            passedTestingsHolder.remove(passedTestingsHolder.lastChild);
+        }
+        for(let i = 0 ; i < allPassedTestings.length; i++){
+            if(allPassedTestings[i].forkId == forkId){
+                const passed = allPassedTestings[i];
+                const item = document.createElement('tr');
+                item.classList.add('.testingsRow');
+                const unitText = document.createElement('td');
+                unitText.classList.add('.unitTextTesting');
+                unitText.innerText = passed.unitId.replace(/ø/g, ' ');
+                const dateText = document.createElement('td');
+                dateText.classList.add('.dateTextTesting');
+                dateText.innerHTML = Date.today().addMilliseconds(-new Date().getTime()).addMilliseconds(passed.startedOn).toString("d.MM.yy,<br/>HH:mm:ss");
+                const durationText = document.createElement('td');
+                durationText.classList.add('.durationTextTesting');
+                durationText.innerText = Date.today().addMilliseconds(-new Date().getTime()).addMilliseconds(passed.duration).toString("HH:mm:ss");
+                const percentText = document.createElement('td');
+                percentText.classList.add('.percentTextTesting');
+                percentText.innerHTML = passed.percent.toString() + "%";
+                const testsText = document.createElement('td');
+                testsText.classList.add('.testsTextTesting');
+                testsText.innerHTML = passed.percent.toString();
+                item.append(unitText);
+                item.append(dateText);
+                item.append(durationText);
+                item.append(percentText);
+                item.append(testsText);
+                passedTestingsHolder.append(item);
+            }
+        }
     }
 }
